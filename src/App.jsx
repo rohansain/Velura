43da71a32352ed05e2bb162c5c0a244747af29bd
rohan <rohansain708@gmail.com>
2025-07-13 09:30:18 +0530

    Initial project setup with Tailwind and Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500 text-white">
+        <h1 className="text-4xl font-bold">🚀 Tailwind + React + Vite!</h1>
+        <h1 className="text-5xl font-bold">Rohan Sain</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
